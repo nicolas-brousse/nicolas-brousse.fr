a964cada303ec4499aca860df2624505e8178525
Nicolas Brousse <nicolas@pantographe.studio>
2017-01-11 14:24:53 +0100

    New version

--- assets/js/app.js
+++
@@ -1,9 +0,0 @@
-setTimeout(function(){
-  var scene    = document.getElementById('background');
-  var parallax = new Parallax(scene);
-
-  scene    = document.getElementById('container');
-  parallax = new Parallax(scene);
-
-  document.getElementById('container').classList.add('show');
-}, 3000);
